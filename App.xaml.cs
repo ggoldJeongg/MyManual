6545c3421a6085a857aab4f0de3cb9e35137e2d5
goldJeong <manage06@naver.com>
2026-01-03 20:09:49 +0900

    Initial commit: WPF Manual project setup

---
+++ App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using OnboardingManual.ViewModels;
+using OnboardingManual.Views;
+
+namespace OnboardingManual
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var view = new OnboardingView
+            {
+                DataContext = new OnboardingViewModel()
+            };
+
+            var window = new Window
+            {
+                Title = "OnboardingManual",
+                Content = view,
+                Width = 1280,
+                Height = 1080,
+                WindowStartupLocation = WindowStartupLocation.CenterScreen
+            };
+
+            window.Show();
+        }
+    }
+}
