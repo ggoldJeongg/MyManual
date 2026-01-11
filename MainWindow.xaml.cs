373e1fbd26462671f48d68aab30bf2dff92988a5
goldJeong <manage06@naver.com>
2026-01-11 16:28:16 +0900

    feat: 온보딩 화면 데이터 분리
    - 헤더 레이아웃 수정
    - 온보딩 화면 내부 임시 데이터 db쪽으로 분리

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,4 @@
 using System.Windows;
-using System.Windows.Controls;
 using MyManual.ViewModels;
 using MyManual.Views;
 
@@ -7,54 +6,19 @@ namespace MyManual;
 
 public partial class MainWindow : Window
 {
-    private OnboardingView? _onboardingView;
-    private ManualView? _manualView;
-    private OnboardingViewModel? _onboardingViewModel;
-    private ManualViewModel? _manualViewModel;
-
     public MainWindow()
     {
         InitializeComponent();
 
-        // ViewModel 초기화
+        // App.xaml.cs에서 설정한 CurrentUser 사용
         if (App.CurrentUser != null)
         {
-            _onboardingViewModel = new OnboardingViewModel(App.CurrentUser);
-            _manualViewModel = new ManualViewModel();
+            var onboardingView = new OnboardingView
+            {
+                DataContext = new OnboardingViewModel(App.CurrentUser)
+            };
 
-            // 이벤트 구독: 온보딩에서 매뉴얼 열기 요청 시
-            _onboardingViewModel.OpenManualRequested += OnOpenManualRequested;
-
-            // View 초기화
-            _onboardingView = new OnboardingView { DataContext = _onboardingViewModel };
-            _manualView = new ManualView { DataContext = _manualViewModel };
-
-            // 시작 화면: 온보딩
-            NavigateToOnboarding();
-        }
-    }
-
-    // 온보딩에서 매뉴얼 열기 요청 시 호출
-    private void OnOpenManualRequested(int manualId)
-    {
-        NavigateToManual(manualId);
-    }
-
-    // 온보딩 화면으로 이동
-    public void NavigateToOnboarding()
-    {
-        MainContent.Content = _onboardingView;
-    }
-
-    // 매뉴얼 화면으로 이동
-    public void NavigateToManual(int? manualId = null)
-    {
-        MainContent.Content = _manualView;
-
-        // 특정 매뉴얼로 이동 요청 시
-        if (manualId.HasValue && _manualViewModel != null)
-        {
-            _manualViewModel.NavigateToManual(manualId.Value);
+            this.Content = onboardingView;
         }
     }
 }
--- Views/OnboardingView.xaml.cs
+++ Views/OnboardingView.xaml.cs
@@ -1,4 +1,3 @@
-using System.Windows;
 using System.Windows.Controls;
 
 namespace MyManual.Views
@@ -8,13 +7,6 @@ namespace MyManual.Views
         public OnboardingView()
         {
             InitializeComponent();
-        }
-
-        // 매뉴얼 버튼 클릭 시 MainWindow의 NavigateToManual 호출
-        private void OnManualButtonClick(object sender, RoutedEventArgs e)
-        {
-            var mainWindow = Window.GetWindow(this) as MainWindow;
-            mainWindow?.NavigateToManual();
         }
     }
 }
