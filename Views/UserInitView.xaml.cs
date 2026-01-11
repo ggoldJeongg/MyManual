98424cb65c4d55f7d76bd4b3cd96fc3256c0f4f0
goldJeong <manage06@naver.com>
2026-01-11 17:23:53 +0900

    feat: 유저 정보 입력 화면 view 구현

---
+++ Views/UserInitView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace MyManual.Views
+{
+    public partial class UserInitView : UserControl
+    {
+        public UserInitView()
+        {
+            InitializeComponent();
+        }
+    }
+}
